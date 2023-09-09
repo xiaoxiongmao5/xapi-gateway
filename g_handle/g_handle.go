4a2ca1ca25cc578663dfabffb9e98030b88b28b5
小熊 <627516430@qq.com>
2023-09-09 22:09:33 +0800

    修改g_handle中响应都返回状态码为200，方便前端统一判断，异常仅根据result值判断

--- g_handle/g_handle.go
+++ g_handle/g_handle.go
@@ -8,27 +8,35 @@ import (
 
 // 状态未授权 401
 func HandlerUnauthorized(c *gin.Context) {
-	c.JSON(http.StatusUnauthorized, gin.H{"result": http.StatusUnauthorized, "msg": "状态未授权"})
+	c.JSON(http.StatusOK, gin.H{"result": http.StatusUnauthorized, "msg": "状态未授权"})
 	c.Abort()
 }
 
 // 禁止状态 403
 func HandlerForbidden(c *gin.Context) {
-	c.JSON(http.StatusForbidden, gin.H{"result": http.StatusForbidden, "msg": "禁止状态"})
+	c.JSON(http.StatusOK, gin.H{"result": http.StatusForbidden, "msg": "禁止状态"})
 	c.Abort()
 }
 
 // 从上下文信息获取信息失败 204
 func HandlerContextError(c *gin.Context, key string) {
-	c.JSON(http.StatusNoContent, gin.H{"result": http.StatusNoContent, "msg": "从上下文信息获取[" + key + "]失败"})
+	c.JSON(http.StatusOK, gin.H{"result": http.StatusNoContent, "msg": "从上下文信息获取[" + key + "]失败"})
 	c.Abort()
 }
 
 // 未知错误 500
 func HandlerServerError(c *gin.Context) {
-	c.JSON(http.StatusInternalServerError, gin.H{"result": http.StatusInternalServerError, "msg": "未知错误"})
+	c.JSON(http.StatusOK, gin.H{"result": http.StatusInternalServerError, "msg": "未知错误"})
 	c.Abort()
 }
+
+// Dubbo加载失败 424
+func HandlerDobboLoadFailed(c *gin.Context, msg string) {
+	c.JSON(http.StatusOK, gin.H{"result": http.StatusFailedDependency, "msg": "Dubbo加载失败: " + msg})
+	c.Abort()
+}
+
+// ——————————————————————以下是业务——————————————————————
 
 // 业务成功 200
 func HandlerSuccess(c *gin.Context, msg string, data any) {
@@ -38,20 +46,6 @@ func HandlerSuccess(c *gin.Context, msg string, data any) {
 	}
 	c.JSON(http.StatusOK, gin.H{"result": 0, "msg": msg, "data": data})
 }
-
-// Dubbo加载失败 424
-func HandlerDobboLoadFailed(c *gin.Context, msg string) {
-	c.JSON(http.StatusFailedDependency, gin.H{"result": http.StatusFailedDependency, "msg": "Dubbo加载失败: " + msg})
-	c.Abort()
-}
-
-// 调用接口失败 400
-func HandlerInvokeError(c *gin.Context) {
-	c.JSON(http.StatusBadRequest, gin.H{"result": http.StatusBadRequest, "msg": "调用接口失败"})
-	c.Abort()
-}
-
-// ——————————————————————以下是业务——————————————————————
 
 // 参数错误 200
 func HandlerParamError(c *gin.Context, paramName string) {
@@ -66,5 +60,11 @@ func HandlerParamError(c *gin.Context, paramName string) {
 // 校验接口失败 200
 func HandlerValidInterfaceFailed(c *gin.Context, msg string) {
 	c.JSON(http.StatusOK, gin.H{"result": 2, "msg": msg})
+	c.Abort()
+}
+
+// 调用接口失败 400
+func HandlerInvokeError(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"result": http.StatusBadRequest, "msg": "调用接口失败"})
 	c.Abort()
 }
